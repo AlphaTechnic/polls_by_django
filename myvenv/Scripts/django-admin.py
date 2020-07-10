173e33a142a3d82205a821deabb393cb6749392d
AlphaTechnic <juho3471@google.com>
2020-07-10 15:48:21 +0900

    2nd Django tutorial practice

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\juho3\desktop\quizlet_app_development\django_practice_\polls_by_django\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
